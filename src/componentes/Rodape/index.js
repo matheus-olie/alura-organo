6b2beb47fbebd8666248d9372a08d46929a35c99
Matheus Olie <104086046+matheus-olie@users.noreply.github.com>
2024-06-17 12:56:05 -0300

    organo

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,40 @@
+import "./Rodape.css"
+
+const Rodape = () => {
+    return(
+ /*       <footer className="footer">
+            <section>
+                <ul>
+                    <li>
+                        <a href="facebook.com" target="_blank">
+                            <img src="/imagens/fb.png" alt="logo facebook"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="twitter.com" target="_blank">
+                            <img src="/imagens/tw.png" alt="logo twitter"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="instragram.com" target="_blank">
+                            <img src="/imagens/ig.png" alt="logo instagram"/>
+                        </a>
+                    </li>
+                </ul> 
+            </section>
+            <section>
+                <img src="/imagens/logo.png" alt="logo"/>
+            </section>
+            <section>
+                <p>Desenvolvido por Alura</p>
+            </section>
+        </footer>
+*/
+
+        <footer className='footer'>
+            <img src="/imagens/Rodapé.png" alt="Rodapé"></img>
+        </footer>
+    )
+}
+
+export default Rodape
